226061d120d2aee2b04331311834a97819470da2
Daniel Gonzalez <daniel.gonzalez.02@gmail.com>
2024-11-26 22:12:22 +0100

    chap3 tp3 hello universe

--- src/HelloUniverse.java
+++ src/HelloUniverse.java
@@ -1,4 +1,13 @@
 public class HelloUniverse {
+    public static void displayPeriodEqualToANumberOfPlanet (int nbPlanet) {
+        switch (nbPlanet) {
+            case 7 -> System.out.println("On sait qu'au 16ème siècle, seules 7 planètes avaient été découvertes");
+            case 8 -> System.out.println("On sait que le nombre de planètes est passé de 7 à 8 au 17ème siècle, mais il a également été réduit de 9 à 8 en 2006");
+            case 9 -> System.out.println("On sait que le nombre de planètes est passé de 8 à 9 au 18ème siècle et ce jusqu'en 2006, où ce nombre a été réduit à 8");
+            default -> System.out.printf("Le programme ne peut pas fournir de résultat pour %d", nbPlanet);
+        }
+    }
+
     public static int calculNbPlanete(int annee) {
   if(annee < 1600 || annee > 2024) {
       return 0;
@@ -32,5 +41,10 @@ public class HelloUniverse {
         message(2006);
         message(2024);
         message(2025);
+
+        displayPeriodEqualToANumberOfPlanet(7);
+        displayPeriodEqualToANumberOfPlanet(8);
+        displayPeriodEqualToANumberOfPlanet(9);
+        displayPeriodEqualToANumberOfPlanet(14);
     }
 }
