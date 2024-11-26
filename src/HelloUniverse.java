5558cc1167acd96d724b933d03c1e413337a70ec
Daniel Gonzalez <daniel.gonzalez.02@gmail.com>
2024-11-26 21:47:51 +0100

    chap3 tp2 hello universe

--- src/HelloUniverse.java
+++ src/HelloUniverse.java
@@ -1,20 +1,36 @@
 public class HelloUniverse {
     public static int calculNbPlanete(int annee) {
-        if(annee < 2006) {
-            return 9;
-        } else {
-            return  8;
-        }
+  if(annee < 1600 || annee > 2024) {
+      return 0;
+  } else if (annee < 1700) {
+      return 7;
+  } else if (annee < 1800) {
+      return 8;
+  } else if (annee < 2006) {
+      return 9;
+  } else {
+      return 8;
+  }
     }
 
     public static void message(int annee) {
         int nbPlanete = calculNbPlanete(annee);
-        System.out.printf("en %d, les planètes du système solaire était au nombre de : %d \n", annee, nbPlanete);
+        if(nbPlanete == 0) {
+            String error = "Le programme ne peut pas fournir de résultat pour l'année %d\n";
+            System.out.printf(error, annee);
+        }else {
+            String message = "en %d, les planètes du système solaire était au nombre de : %d \n";
+            System.out.printf(message, annee, nbPlanete);
+        }
     }
 
     public static void main (String... args) {
-        message(2003);
-        message(2014);
+        message(1599);
+        message(1600);
+        message(1700);
+        message(1800);
         message(2006);
+        message(2024);
+        message(2025);
     }
 }
